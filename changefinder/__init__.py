fb9f8153978ab3efecceec4c553228915df68b0e
aihara <s.aihara@gmail.com>
2013-12-29 00:43:41 +0900

    SDAR一応追加

--- changefinder/__init__.py
+++ changefinder/__init__.py
@@ -49,13 +49,47 @@ x=np.random.rand(30)
 s.update(0.22,x)
 """
 
+
+def LevinsonDurbin(r, lpcOrder):
+    """
+    from http://aidiary.hatenablog.com/entry/20120415/1334458954
+    """
+    a = np.zeros(lpcOrder + 1,dtype=np.float64)
+    e = np.zeros(lpcOrder + 1,dtype=np.float64)
+
+    a[0] = 1.0
+    a[1] = - r[1] / r[0]
+    e[1] = r[0] + r[1] * a[1]
+    lam = - r[1] / r[0]
+
+    for k in range(1, lpcOrder):
+        lam = 0.0
+        for j in range(k + 1):
+            lam -= a[j] * r[k + 1 - j]
+        lam /= e[k]
+
+        U = [1]
+        U.extend([a[i] for i in range(1, k + 1)])
+        U.append(0)
+
+        V = [0]
+        V.extend([a[i] for i in range(k, 0, -1)])
+        V.append(1)
+
+        a = np.array(U) + lam * np.array(V)
+        e[k + 1] = e[k] * (1.0 - lam * lam)
+
+    return a, e[-1]
+
+
 class _SDAR_1Dim(object):
-    def __init__(self,r = 0.9,term = 30,lag=8):
+    def __init__(self, r, term,order = 5):
         self._r = r
-        self._mu = 0
+        self._mu = np.random.random()
+        self._sigma = np.random.random()
         self._term = term
-        self._lag = lag
-        self._c = np.zeros(self._lag)
+        self._order = order
+        self._c = np.zeros(self._term+1)
 
     def update(self,x,term):
         def outlier_score(residuals,x):
@@ -65,16 +99,17 @@ class _SDAR_1Dim(object):
 
         term = np.array(term)
         self._mu = (1 - self._r) * self._mu + self._r * x
-        for i in range(self._lag):
-            self._c[i] = (1-self._r)*self._c[i]+self._r * (x-self._mu) * (term[-1-i]-self._mu)
-        ar = sm.tsa.AR(np.array(term))
-        result = ar.fit(maxlag=self._lag,trend="nc")
-        what=result.params
-        xhat = np.dot(what,(term[::-1][:self._lag]  - self._mu))+self._mu
-        return outlier_score(result.resid,x-xhat)
+        for i in range(1,self._term):
+            self._c[i] = (1-self._r)*self._c[i]+self._r * (x-self._mu) * (term[-i]-self._mu)
+        self._c[0] = (1-self._r)*self._c[0]+self._r * (x-self._mu)*(x-self._mu)
 
-class ChangeFinder(object):
-    def __init__(self, r = 0.5,term=30, smooth=7,lag=8):
+        what,e = LevinsonDurbin(self._c,self._order)
+        xhat = np.dot(-what[1:],(term[::-1][:self._order]  - self._mu))+self._mu
+        self._sigma = (1-self._r)*self._sigma + self._r * (x-xhat) * (x-xhat)
+        return -math.log(math.exp(-0.5 *(x-xhat)**2/self._sigma)/((2 * math.pi)**0.5 * self._sigma**0.5))
+
+class ChangeFinderSDAR(object):
+    def __init__(self, r = 0.5, term=10, smooth=7):
         self._smooth = smooth
         self._term = term
         self._r = r
@@ -103,17 +138,18 @@ class ChangeFinder(object):
 
 
     def update(self,x):
+        score = 0
         if len(self._ts) == self._term:#第一段学習
-            self._add_one(self._sdar_first.update(x,self._ts),self._first_scores,self._smooth)
+            score = self._sdar_first.update(x,self._ts)
+            self._add_one(score,self._first_scores,self._smooth)
         self._add_one(x,self._ts,self._term)
-
         second_target = None
         if len(self._first_scores) == self._smooth:#平滑化
             second_target = self._smoothing(self._first_scores)
         if second_target and len(self._smoothed_scores) == self._term:#第二段学習
-            self._add_one(self._sdar_second.update(second_target,self._smoothed_scores),
+            score = self._sdar_second.update(second_target,self._smoothed_scores)
+            self._add_one(score,
                           self._second_scores,self._smooth2)
-
         if second_target:
             self._add_one(second_target,self._smoothed_scores, self._term)
         if len(self._second_scores) == self._smooth2:
@@ -122,7 +158,7 @@ class ChangeFinder(object):
             return 0.0
 
 
-class _ChangeFinderOld(object):
+class _ChangeFinder(object):
     def __init__(self,term = 30, smooth = 7, order = (1,0,0)):
         assert smooth > 2, "term must be 3 or more."
         assert term > smooth, "term must be more than smooth"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from setuptools import setup
-version="0.03"
+version="0.02"
 README = os.path.join(os.path.dirname(__file__),"README.rst")
 long_description = open(README).read() + '\n\n'
 setup(name="changefinder",
