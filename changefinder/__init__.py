6485f6ad3545174d840e9f6b8eb2b442cbb82dfa
aihara <s.aihara@gmail.com>
2014-01-16 03:30:30 +0900

    畳み込んだあと割るの忘れてた

--- changefinder/__init__.py
+++ changefinder/__init__.py
@@ -55,9 +55,23 @@ class _SDAR_1Dim(object):
         what,e = LevinsonDurbin(self._c,self._order)
         xhat = np.dot(-what[1:],(term[::-1]  - self._mu))+self._mu
         self._sigma = (1-self._r)*self._sigma + self._r * (x-xhat) * (x-xhat)
-        return -math.log(math.exp(-0.5 *(x-xhat)**2/self._sigma)/((2 * math.pi)**0.5 * self._sigma**0.5))
+        return -math.log(math.exp(-0.5 *(x-xhat)**2/self._sigma)/((2 * math.pi)**0.5 * self._sigma**0.5)),xhat
 
-class ChangeFinder(object):
+class _ChangeFinderAbstract(object):
+    def _add_one(self,one,ts,size):
+        ts.append(one)
+        if len(ts)==size+1:
+            ts.pop(0)
+
+    def _smoothing(self,ts):
+        ts = np.array(ts)
+        return np.convolve(ts,self._convolve,'valid')[0]/self._smooth
+
+    def _smoothing2(self,ts):
+        ts = np.array(ts)
+        return np.convolve(ts,self._convolve2,'valid')[0]/self._smooth2
+
+class ChangeFinder(_ChangeFinderAbstract):
     def __init__(self, r = 0.5, order = 1, smooth=7):
         assert order > 0, "order must be 1 or more."
         assert smooth > 2, "term must be 3 or more."
@@ -74,42 +88,30 @@ class ChangeFinder(object):
         self._sdar_first = _SDAR_1Dim(r,self._order)
         self._sdar_second = _SDAR_1Dim(r,self._order)
 
-    def _add_one(self,one,ts,size):
-        ts.append(one)
-        if len(ts)==size+1:
-            ts.pop(0)
-
-    def _smoothing(self,ts):
-        ts = np.array(ts)
-        return np.convolve(ts,self._convolve,'valid')[0]
-
-    def _smoothing2(self,ts):
-        ts = np.array(ts)
-        return np.convolve(ts,self._convolve2,'valid')[0]
-
-
     def update(self,x):
         score = 0
+        predict = x
+        predict2 = 0
         if len(self._ts) == self._order:#第一段学習
-            score = self._sdar_first.update(x,self._ts)
+            score,predict = self._sdar_first.update(x,self._ts)
             self._add_one(score,self._first_scores,self._smooth)
         self._add_one(x,self._ts,self._order)
         second_target = None
         if len(self._first_scores) == self._smooth:#平滑化
             second_target = self._smoothing(self._first_scores)
         if second_target and len(self._smoothed_scores) == self._order:#第二段学習
-            score = self._sdar_second.update(second_target,self._smoothed_scores)
+            score,predict2 = self._sdar_second.update(second_target,self._smoothed_scores)
             self._add_one(score,
                           self._second_scores,self._smooth2)
         if second_target:
             self._add_one(second_target,self._smoothed_scores, self._order)
         if len(self._second_scores) == self._smooth2:
-            return self._smoothing2(self._second_scores)
+            return self._smoothing2(self._second_scores),predict
         else:
-            return 0.0
+            return 0.0,predict
 
 
-class ChangeFinderARIMA(object):
+class ChangeFinderARIMA(_ChangeFinderAbstract):
     def __init__(self,term = 30, smooth = 7, order = (1,0,0)):
         assert smooth > 2, "term must be 3 or more."
         assert term > smooth, "term must be more than smooth"
@@ -125,11 +127,6 @@ class ChangeFinderARIMA(object):
         self._smooth2 = int(round(self._smooth/2.0))
         self._convolve2 = np.ones(int(round(self._smooth2)))
 
-    def _add_one(self,one,ts,size):
-        ts.append(one)
-        if len(ts)==size+1:
-            ts.pop(0)
-
     def _calc_outlier_score(self,ts,target):
         def outlier_score(residuals,x):
             m = residuals.mean()
@@ -139,39 +136,34 @@ class ChangeFinderARIMA(object):
         arima_model = sm.tsa.ARIMA(ts,self._order)
         result = arima_model.fit(disp=0)
         pred = result.forecast(1)[0][0]
-        return outlier_score(result.resid,x=pred-target)
+        return outlier_score(result.resid,x=pred-target),pred
 
-    def _smoothing(self,ts):
-        ts = np.array(ts)
-        return np.convolve(ts,self._convolve,'valid')[0]
-
-    def _smoothing2(self,ts):
-        ts = np.array(ts)
-        return np.convolve(ts,self._convolve2,'valid')[0]
-
-    def update(self,sample):
+    def update(self,x):
+        score = 0
+        predict = x
+        predict2 = 0
         if len(self._ts) == self._term:#第一段学習
             try:
-                self._add_one(self._calc_outlier_score(self._ts,sample),self._first_scores,self._smooth)
+                score,predict = self._calc_outlier_score(self._ts,x)
+                self._add_one(score,self._first_scores,self._smooth)
             except:
-                self._add_one(sample,self._ts,self._term)
-                return 0
-        self._add_one(sample,self._ts,self._term)
-
+                self._add_one(x,self._ts,self._term)
+                return 0,predict
+        self._add_one(x,self._ts,self._term)
         second_target = None
         if len(self._first_scores) == self._smooth:#平滑化
             second_target = self._smoothing(self._first_scores)
-
         if second_target and len(self._smoothed_scores) == self._term:#第二段学習
             try:
-                self._add_one(self._calc_outlier_score(self._smoothed_scores,second_target),
+                score,predict2 = self._calc_outlier_score(self._smoothed_scores,second_target)
+                self._add_one(score,
                               self._second_scores,self._smooth2)
             except:
                 self._add_one(second_target,self._smoothed_scores, self._term)
-                return 0
+                return 0,predict
         if second_target:
             self._add_one(second_target,self._smoothed_scores, self._term)
         if len(self._second_scores) == self._smooth2:
-            return self._smoothing2(self._second_scores)
+            return self._smoothing2(self._second_scores),predict
         else:
-            return 0.0
+            return 0.0,predict
